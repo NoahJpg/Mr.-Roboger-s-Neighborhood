dcce5323dab43c0a635158cdfb8c01b47bc1711f
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-01-20 16:47:32 -0800

    WIP: add UI logic to help output results to user

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,4 @@
-
-
 // Business Logic
-// function splitIntoArray(number) {
-//   let userInput = parseInt(document.querySelector("input#userInput").value);
-//   let numberArray = [];
-//   numberArray.push(number); 
-//   return numberArray;
-// }
-
-// function addNumsBelowInput(number) {
-//   let numArray = splitIntoArray()
-//   for (let i = 0; i <= number; i++)
-//     numArray += i;
-//   numArray = numArray.split("1");
-
-//   return numArray;
-// }
-
 function myNeighbor() {
   let number = document.getElementById("userNum").value;
   let result = [];
@@ -36,43 +18,17 @@ function myNeighbor() {
     }
   }
 }
+
+
+// UI Logic
 function outputResults () {
   let resultString = "";
   for (let i = 0; i <= result.length; i++) {
       resultString += result[i];
+    } 
       document.getElementById("output").innerHTML = resultString;
-    }
   }
 
-// UI Logic
 window.addEventListener("load", function() {
   form.addEventListener("submit", myNeighbor, outputResults);
 })
-
-
-// const array = addNumsBelowInput(number);
-
-// function replaceOnesWithBeep(number) {
-//   let oneNumber = addNumsBelowInput(number);
-//   for (let i = 0; i <= number; i++) {
-//     if(i === 1);
-//     return "Beep!"
-//   }
-// }
-
-// function replaceTwosWithBoop(number) {
-//   for (let i = 0; i <= number; i++) {
-//     if(i === 2);
-//     return "Boop!"
-//   }
-// }
-
-// function replaceThreesWithNeighbor(number) {
-//   for (let i = 0; i <= number; i++) {
-//     if(i === 3);
-//     return "Won't you be my Neighbor?"
-//   }
-// }
-
-
-
