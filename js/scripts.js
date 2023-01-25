106091f35bc0daf8543321c79a0d0adb0b109dd4
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-01-25 14:55:20 -0800

    fix output function and result order to output correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,37 @@
 // Business Logic
-function myNeighbor() {
+function myNeighbor(event) {
+  event.preventDefault();
+  console.log(event + " this is the event info")
   let number = document.getElementById("userNum").value;
   let result = [];
   for (let i = 0; i <= number; i++) {
     let number = i.toString();
-    if (number.includes("1")) {
-      result.push("Beep!")
+
+    if (number.includes("3")) {
+      result.push("Won't you be my neighbor?");
     }
     else if (number.includes("2")) {
       result.push("Boop!");
     }
-    else if (number.includes("3")) {
-      result.push("Won't you be my neighbor?");
-    }
-    else {
+    else if (number.includes("1")) {
+      result.push("Beep!")
+    } else {
       result.push(i);
     }
   }
-
-function outputResults () {
+  console.log(result + "-result")
+  outputResults(result);
+}
+function outputResults(result) {
   let resultString = "";
-  for (let i = 0; i <= result.length; i++) {
+  for (let i = 0; i < result.length; i++) {
       resultString += result[i]; + "<br>";
     } 
       document.getElementById("output").innerHTML = resultString;
   }
-}
+
 // UI Logic
 window.addEventListener("load", function() {
-  const form = document.getElementById("countInput");
+  const form = document.getElementById("form");
   form.addEventListener("submit", myNeighbor);
-  load.preventDefault();
-  return outputResults();
 })
